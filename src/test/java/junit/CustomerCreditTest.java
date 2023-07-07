e5119786f63f702b71346208258b6d096955bc19
cinematografie <kristinadruin@gmail.com>
2023-07-07 12:25:50 +0100

    KD - update file locations

--- src/test/java/junit/CustomerCreditTest.java
+++ src/test/java/junit/CustomerCreditTest.java
@@ -1,4 +1,5 @@
-import junit.CustomerCredit;
+package junit;
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
--- src/test/java/junit/CustomerRewardScoreTest.java
+++ src/test/java/junit/CustomerRewardScoreTest.java
@@ -1,4 +1,5 @@
-import junit.CustomerRewardScore;
+package junit;
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
